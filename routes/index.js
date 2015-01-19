b42d937a015eb12fe7f3875d392a8b3a80bb0a96
YongJunKim <healu1423@gmail.com>
2015-01-19 15:10:48 +0900

    <% include content %>를 활용해서 탭 구
    html 상속을 통해서  md파일 출력하는 부분 분리해서 탭 구현 했습니다. 아쉬운 점이 있다면 js 파일을 좀더 다듬어야할 꺼
    같네요.

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,55 @@
 var express = require('express');
 var fs = require('fs');
 var markdown = require('markdown').markdown;
-var path = './projects';
+var abpath = './projects';
 var router = express.Router();
+
+
+/* READ dir list */
+var dirs =[];
+var projects = fs.readdirSync(abpath);
+
+for (i in projects){
+    var filename = projects[i];
+    var stat = fs.statSync(abpath + '/' + filename);
+     
+    if (stat.isDirectory()) {
+            dirs.push(filename);
+    }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var projects = fs.readdirSync(path);
-    console.log(projects);
-    var dirs =[];
-    var mds = []; 
+    var mds = [];
+    var mdsname=[];
+    mdsname.push("README.md")
+    var content = fs.readFileSync('./README.md','utf-8');
+    var html = markdown.toHTML(content);
+    mds.push(html);
+
+	res.render('index',{'dirs':dirs, 'mds':mds, 'mdname':mdsname});
+});
+
+router.get('/:dir', function(req, res){
+    var mdpath = abpath + "/"+ req.params.dir;
+    var projects = fs.readdirSync(mdpath);
+    var mds= [];
+    var mdsname= [];
+
     for (i in projects){
-        var filename = projects[i];
-        var stat = fs.statSync(path + '/' + filename);
+    var filename = projects[i];
+    
+    var stat = fs.statSync(mdpath + '/' + filename);
      
-        if (stat.isDirectory()) {
-            dirs.push(filename);
-        }else{
-			var content = fs.readFileSync(path+'/'+filename,'utf-8');
-			var html = markdown.toHTML(content);
+        if (stat.isFile()) {
+            var content = fs.readFileSync(mdpath + '/' + filename,'utf-8');
+            var html = markdown.toHTML(content);
             mds.push(html);
-        }   
-        //res.send(i);
+            mdsname.push(filename);
+        }
     }
-	res.render('index',{'dirs':dirs, 'mds':mds});
+
+    res.render('index',{'dirs':dirs, 'mds':mds, 'mdname':mdsname});
 });
 
 module.exports = router;
