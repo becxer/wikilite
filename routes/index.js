4f4266410ec48f0476629f408f022dfbdc150ea4
YongJunKim <healu1423@gmail.com>
2015-01-20 00:42:43 +0900

    주석처

--- routes/index.js
+++ routes/index.js
@@ -40,12 +40,13 @@ router.get('/:dir', function(req, res){
     var mds= [];
     var mdsname= [];
 
+    //파일 목록을 읽어들임
     for (i in projects){
     var filename = projects[i];
-    
     var stat = fs.statSync(mdpath + '/' + filename);
-    var ext = filename.substring(filename.lastIndexOf(".") + 1);
 
+    //확장자가 md인 파일만 읽도록 확장자를 자른후 확인
+    var ext = filename.substring(filename.lastIndexOf(".") + 1);
         if (stat.isFile() && ext == 'md') {
             var content = fs.readFileSync(mdpath + '/' + filename,'utf-8');
             var html = markdown.toHTML(content);
@@ -65,6 +66,7 @@ router.get('/:dir/:md', function(req,res){
     var mds = [];
     var mdsname= [];
 
+    //올바른 상위 디렉토리 명인지 파일이 존재하는지 확인
     if(dirs.indexOf(dirname) != -1 && fs.existsSync(mdpath)){
         var content = fs.readFileSync(mdpath,'utf-8');
         var html = markdown.toHTML(content);
@@ -82,5 +84,6 @@ router.get('/:dir/:md', function(req,res){
 router.get('//README',function(req,res){
     res.redirect('/');
 });
+// 메인페이지 README url 연결시 main으로 리다이렉트
 
 module.exports = router;
