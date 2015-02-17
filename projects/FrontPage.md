cdedbaeb5d72f495bb1c4b93d16bb211957180ed
YongJunKim <healu1423@gmail.com>
2015-02-18 06:21:31 +0900

    추가 및 수정 기능 구
    Code Mirror를 이

---
+++ projects/FrontPage.md
@@ -0,0 +1,88 @@
+wikilite
+======
+
+##What is wikilite
+ * The project 'wikilite' starts on the think that wants to use simple todo list editor having wiki's features. It uses 'markdown' grammar like git wiki, and specialized for individual uses.
+
+##Development issues
+[to go github issues, click me](https://github.com/becxer/wikilite/issues)
+
+##History
+
+[wikilite-0.0.2] YongJunKim, HyeokOh, SeoHyeonBaek has remaked with node.js and kick out php.
+
+[wikilite-0.0.1] SeoHyeonBaek established with codeigniter 
+
+##How To Install/Run (Example for Ubuntu/Debian)
+```
+ 1)install node.js
+ $sudo apt-get install nodejs
+ $sudo apt-get install npm
+ 
+ 2)clone git
+ $git clone https://github.com/becxer/wikilite.git
+ $cd wikilite
+ $npm install
+ $npm start
+
+ 3)check in browser
+ http://localhost:3000 or http://[YOURADDRESS]:3000
+ 
+```
+
+##Functions of Wikilite
+ * Documentation
+  * markdown rendering
+
+##Features of wikilite
+ * It does not use any DBMS at all.
+ * Uses markdown grammar.
+
+
+--------------------------------------------------------------
+
+##wikilite 란?
+ * wikilite는 위키같은 할일 메모장을 사용하고 싶은 마음에서 시작되었습니다. 개인화된 위키를 지향하며 markdown문법을 사용합니다.
+
+##개발 이슈
+[개발 이슈보기는 이곳을 클릭하세요](https://github.com/becxer/wikilite/issues)
+
+##개발 이력
+
+[wikilite-0.0.6] YongJunKim 디렉토리 페이지시 네비게이션바 add 버튼을 통해서 파일 추가 가능 하도록 함
+                                
+[wikilite-0.0.5] YongJunKim CodeMirror를 탑재해 제목 클릭시 md파일 수정 가능 하도록 함
+                                
+[wikilite-0.0.4] YongJunKim 폴더별 md파일이 최근 추가 및 수정 순으로 정렬되도록 함
+                                
+[wikilite-0.0.3] YongJunKim 폴더별 탭구현
+
+[wikilite-0.0.2] YongJunKim, HyeokOh, SeoHyeonBaek 이 PHP를 아웃 시키고, 노드로 다시 만듬
+
+[wikilite-0.0.1] SeoHyeonBaek 이 코드이그나이터로 만듬
+
+##설치 및 실행방법(우분투/데비안 리눅스 기준)
+
+```
+ 1)node.js 설치
+ $sudo apt-get install nodejs
+ $sudo apt-get install npm
+ 
+ 
+ 2)git 클론
+ $git clone https://github.com/becxer/wikilite.git
+ $cd wikilite
+ $npm install
+ $nodejs start ./bin/www
+
+ 3)브라우저에서 확인하기
+ http://localhost:3000 or http://[YOURADDRESS]:3000
+```
+
+##wikilite 의 기능
+ * 문서 작성
+  * Markdown 렌더링
+
+##wikilite의 특징
+ * DB를 전혀사용하지 않는다.
+ * Markdown문법을 사용한다.
--- routes/index.js
+++ routes/index.js
@@ -47,14 +47,14 @@ function intro_search(path) {
 //------------------------------------------------------------------------------
 
 function insList(List,object,pathArr,order){    //객체를 통해서 같은 레벨 단위로 디렉토리명 리스트를 만드는 함수
-        List.push(Object.keys(object));         //현재 레벨의 객체들의 디렉토리명을 리스트에 푸쉬
-        
-            for(i in object){
-                if(escape(i) == escape(pathArr[order]) &&
-                    order <= pathArr.length)    //url을 통해서 다음 디렉토리를 찾아낸 이후 그 내부 객체를 찾아 들어감
-                    insList(List,object[i],pathArr,order+1);
-            }
+    List.push(Object.keys(object));             //현재 레벨의 객체들의 디렉토리명을 리스트에 푸쉬
+
+    for(i in object){
+        if(escape(i) == escape(pathArr[order]) &&
+            order <= pathArr.length)           //url을 통해서 다음 디렉토리를 찾아낸 이후 그 내부 객체를 찾아 들어감
+            insList(List,object[i],pathArr,order+1);
     }
+}
 
 //------------------------------------------------------------------------------
 
@@ -75,23 +75,13 @@ function init(req,list) {
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var mds = [];
-    var mdsname =[];
     var links = [];
 
-    console.log("~~Mistaken text.~~");
-    mdsname.push("README")
-    var content = fs.readFileSync('./README.md','utf-8');
-    var html = marked(content);
+    var content = marked(fs.readFileSync('./projects/FrontPage.md','utf-8'));
+    var html = {name:"FrontPage", content:content};
     mds.push(html);
     
-	res.render('index',{'dirs':dirs, 'mds':mds, 'mdname':mdsname, 'dirList':""});
-});
-
-/* GET README.md url page */
-router.get('//README',function(req,res){
-    
-    // 메인페이지 README url 연결시 main으로 리다이렉트
-    res.redirect('/');
+	res.render('index',{'dirs':dirs, 'dirList':"", 'add':"", 'mds':mds});
 });
 
 /* Get Dirs reset */
@@ -103,6 +93,45 @@ router.get('/set', function(req,res){
     res.redirect('/');
 });
 
+
+
+
+/* Get add Page */
+router.get('/add', function(req,res){
+    var path = req.query.path;
+   res.render('add',{'dirs':dirs, 'dirList':"", 'add':"", 'path':path}); 
+});
+
+
+/* Get Edit Page */
+router.all('/edit', function(req,res){
+    var path = abpath+req.query.path+".md";
+    var content = fs.readFileSync(path,'utf-8');
+
+    res.render('edit',{'dirs':dirs, 'dirList':"", 'add':"", 'content':content, 'path':path});        
+});
+
+/* Post Save Page */
+router.post('/save', function(req,res){
+    //add, edit을 구별해서 path를 저장
+    var path = (req.body.name == undefined) ? req.body.path : req.body.path+"/"+req.body.name+".md";
+    var content = req.body.content;
+
+    fs.writeFile(path,content,function(err){
+        if(err) throw err;
+    });
+
+    var filename = path.substring(path.lastIndexOf('/'));
+    path = path.replace(filename,'');                   //파일이름 제거
+    path = path.substring(10);                          //./projects 제거
+
+    
+    res.redirect((path == '') ? '/': path);
+});
+
+
+
+
 /* GET MD in url page && Directory View page */
 router.get('/*', function(req,res){
     var path = decodeURI(abpath + req.path);    //한글 디코딩
@@ -111,19 +140,11 @@ router.get('/*', function(req,res){
     var mds = [];
     var mdsname= [];
     var dirList = [];
+    var add = "";
 
     init(req,dirList);
 
-    if(fs.existsSync(path+".md"))       //해당하는 md파일이 존재하는지 확인
-    {
-        path += ".md";
-        var content = fs.readFileSync(path,'utf-8');
-        var html = marked(content);
-
-        mds.push(html);
-        mdsname.push(filename);    
-    } 
-    else if(fs.existsSync(path))         //해당하는 디렉토리가 존재하는지 확인
+    if(fs.existsSync(path))         //해당하는 디렉토리가 존재하는지 확인
     {
         var projects = fs.readdirSync(path);
 
@@ -136,18 +157,34 @@ router.get('/*', function(req,res){
             var ext = filename.substring(filename.lastIndexOf(".") + 1);
             
             if (stat.isFile() && ext == 'md') {
-                var content = fs.readFileSync(path + '/' + filename,'utf-8');
-                var html = marked(content);
-            
+                var content = marked(fs.readFileSync(path + '/' + filename,'utf-8'));
+                var html = {name:filename.split(".",1), content:content, mtime:stat.mtime};
+                
                 mds.push(html);
-                mdsname.push(filename.split(".", 1));
             }
         }
+
+        add = "<li><a href=/add?path="+path+">add</a><li>"
     }
+    else if(fs.existsSync(path+".md"))       //해당하는 md파일이 존재하는지 확인
+    {
+        var filename = path.split("/");
+        path += ".md";
+        var stat = fs.statSync(path);
+        var content = marked(fs.readFileSync(path,'utf-8'));
 
-    res.render('index',{'dirs':dirs, 'mds':mds, 'mdname':mdsname, 'dirList':dirList});        
+
+        var html = {name:filename[filename.length-1], content:content, mtime:stat.mtime};
+
+        mds.push(html);  
+    } 
+
+    //최근 추가 및 수정 순으로 정렬
+    mds.sort(function(a,b){
+        return a.mtime > b.mtime ? -1 : a.mtime < b.mtime ? 1 : 0;
+    });
+    res.render('index',{'dirs':dirs, 'dirList':dirList, 'add':add, 'mds':mds });        
 });
-
 
 
 module.exports = router;
