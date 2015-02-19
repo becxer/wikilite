ac366597a1504f52927ac9c5a283e948eba2b071
YongJunKim <healu1423@gmail.com>
2015-02-19 16:31:43 +0900

    Filter View
    /* Get View Page */에서 get방식을 통해서 필터링

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var marked = require('marked');
 var router = express.Router();
 var abpath = './projects';
 
+//------------------------------------------------------------------------------
 /* READ dir list */
 var dirs =[];
 
@@ -21,55 +22,6 @@ function insdirs(){
 }
 
 insdirs();
-
-// 모듈로 뺼것!!------------------------------------------------------[TODO]
-
-// recursive func: find dir and make object
-function intro_search(path) {
-    var tempObj = new Object();         //현재 경로에 dir을 담을 임시 객체
-    var projects = fs.readdirSync(path);//현재 경로에 포함하는 file list GET(file,dir포함)
-
-    for (i in projects) {
-        var tempPath = path + "/" + projects[i];
-        var stat = fs.statSync(tempPath);
-
-        if (stat.isFile()) continue;    //현재 확인하는 file이 isNotDir이면 continue
-        if(projects[i] != ".git")    
-            tempObj[projects[i]] = intro_search(tempPath);
-        
-    }
-
-
-    return tempObj;     //완료된 임시 object 리턴
-}
-
-
-//------------------------------------------------------------------------------
-
-function insList(List,object,pathArr,order){    //객체를 통해서 같은 레벨 단위로 디렉토리명 리스트를 만드는 함수
-    List.push(Object.keys(object));             //현재 레벨의 객체들의 디렉토리명을 리스트에 푸쉬
-
-    for(i in object){
-        if(escape(i) == escape(pathArr[order]) &&
-            order <= pathArr.length)           //url을 통해서 다음 디렉토리를 찾아낸 이후 그 내부 객체를 찾아 들어감
-            insList(List,object[i],pathArr,order+1);
-    }
-}
-
-//------------------------------------------------------------------------------
-
-function init(req,list) {   //
-    var rootobj = intro_search(abpath);     //json 방식으로 해당 URL 안에 들어있는 폴더를 서치intro_search(abpath);
-
-    var pathArr = decodeURIComponent(req.path).split('/');
-    pathArr.shift();
-
-    var dirlist = [];
-    insList(dirlist,rootobj,pathArr,0);
-
-    list.push(pathArr,dirlist);
-}
-
 //------------------------------------------------------------------------------
 function findFile(path, mds, filter) {
     var files = fs.readdirSync(path);
@@ -82,21 +34,17 @@ function findFile(path, mds, filter) {
         if (file.isFile() && ext == 'md')
         {
           var content = marked(fs.readFileSync(path + '/' + filename,'utf-8'));
-          var html = {'name':filename.split(".")[0], 'content':content, 'path':path+'/'+filename, 'mtime':file.mtime};
-                
-          mds.push(html);
+          mds.push({'name':filename.split(".")[0], 'content':content, 'path':path+'/'+filename, 'mtime':file.mtime});
         } 
         else if(file.isDirectory()) 
         {
-          filter.push({'name':filename,'path':path.substring(10)+'/'+filename});
+          filter.push({'name':filename,'path':'?filter='+path.substring(10)+'/'+filename});
           findFile(path+'/'+filename, mds, filter);
         }
     }
 }
-
-
-
 //------------------------------------------------------------------------------
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -152,78 +100,46 @@ router.post('/save', function(req,res){
 
 
 
+/* Get View Page */
 router.get('/:category', function(req,res){
     var path = decodeURI(abpath+'/'+req.params.category);
-    var filter = [];
     var mds = [];
+    var filter = [];
     var add = "<a href=/add?path="+path+">add</a>";
     
-    findFile(path,mds,filter);
+    if(req.query.filter == undefined) 
+        findFile(path,mds,filter);
+    else
+    {
+        path = decodeURI(abpath+req.query.filter);
+        filter = [{'name':'back', 'path':'/'+req.params.category}];
+        console.log(filter);
+        findFile(path,mds,filter);
+    }
 
     mds.sort(function(a,b){
         return a.mtime > b.mtime ? -1 : a.mtime < b.mtime ? 1 : 0;
     });
-    
-    console.log(mds,filter);
 
     res.render('index',{'dirs':dirs, 'filters':filter, 'add':add, 'mds':mds});
 });
 
-
-
-
-/* GET MD in url page && Directory View page */
+/* GET MD in url page */
 router.get('/*', function(req,res){
-    var path = decodeURI(abpath + req.path);    //한글 디코딩
+    var path = decodeURI(abpath+req.path+'.md');    //한글 디코딩
     var filename = decodeURI(req.path.substring(req.path.lastIndexOf('/')+1)); //md파일 이름 잘라내기
-
-    var filter = [];
     var mds = [];
-    var dirList = [];
+    var filter = [];
     var add = "";
 
-    init(req,dirList);
-
-    console.log(dirList);
-    if(fs.existsSync(path))         //해당하는 디렉토리가 존재하는지 확인
+    if(fs.existsSync(path))       //해당하는 md파일이 존재하는지 확인
     {
-        var projects = fs.readdirSync(path);
-
-        //파일 목록을 읽어들임
-        for (i in projects){
-            var filename = projects[i];
-            var stat = fs.statSync(path + '/' + filename);
-
-        //확장자가 md인 파일만 읽도록 확장자를 자른후 확인
-            var ext = filename.substring(filename.lastIndexOf(".") + 1);
-            
-            if (stat.isFile() && ext == 'md') {
-                var content = marked(fs.readFileSync(path + '/' + filename,'utf-8'));
-                var html = {name:filename.split(".",1), content:content, mtime:stat.mtime};
-                
-                mds.push(html);
-            }
-        }
-
-        add = "<a href=/add?path="+path+">add</a>"
-    }
-    else if(fs.existsSync(path+".md"))       //해당하는 md파일이 존재하는지 확인
-    {
-        var filename = path.split("/");
-        path += ".md";
-        var stat = fs.statSync(path);
+        var file = fs.statSync(path);
         var content = marked(fs.readFileSync(path,'utf-8'));
 
+        mds.push({'name':filename, 'content':content, 'path':path, 'mtime':file.mtime});  
+    }
 
-        var html = {name:filename[filename.length-1], content:content, mtime:stat.mtime};
-
-        mds.push(html);  
-    } 
-
-    //최근 추가 및 수정 순으로 정렬
-    mds.sort(function(a,b){
-        return a.mtime > b.mtime ? -1 : a.mtime < b.mtime ? 1 : 0;
-    });
     res.render('index',{'dirs':dirs, 'filters':filter, 'add':add, 'mds':mds });        
 });
 
