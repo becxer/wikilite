11dfb9f867562696d616eae31edf315c1cc5ab6c
BaekSeoHyeon <whitewest87@gmail.com>
2015-06-02 02:07:45 +0900

    cfctrl 이 자동으로 conf/wikiite.conf 파일을 읽어서 설정하게끔 제작중. 현재 facebook login 에 필요한 키를 자동등록 하게끔 만드는중

--- ctrls/cfctrl.js
+++ ctrls/cfctrl.js
@@ -2,8 +2,11 @@
 var fs = require('fs');
 
 //load wikilite.conf
+console.log("loading wikilite.conf");
 conf_obj = {};
+
 cf_str = fs.readFileSync('conf/wikilite.conf','utf-8').split('\n');
+console.log(cf_str);
 for(i in cf_str){
 	var oneline = cf_str[i].trim();	
 	if(oneline.length > 0 && oneline[0] != '#'){
@@ -13,13 +16,21 @@ for(i in cf_str){
 	}
 }
 
+var setConfs_ = function (){
+	for (key in conf_obj){
+		console.log(key);
+		//페북 키값을 설정한 값으로 교체시킨다..
+	}
+}
+
 module.exports = {
 	getAll : function(){
 		return conf_obj;
-	}
-	,	
+	},	
 	getValue : function(key){
 		return conf_obj[key];
-	}
+	},
+	setConfs : setConfs_
 };
 
+setConfs_()
