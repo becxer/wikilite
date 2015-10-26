a118f7b9393102521d36dc9b96b30e57a0f4d2d5
becxer <whitewest87@gmail.com>
2015-10-26 22:48:49 +0900

    modify examples of wikilite [filter,category] to [chapter,book]

--- mdroot/dummy_filters/filter2/filter2.md
+++
@@ -1,3 +0,0 @@
-This feed shows how to filter works.
-
-If you see all feed in this category. It may work nice.
--- routes/preload.js
+++ routes/preload.js
@@ -4,31 +4,31 @@
 // @ e-mail becxer87@gmail.com
 
 pre_obj = {
-	front_title : '',
-	add_title : '글작성하기',
-	edit_title : '글수정하기',
-	front_page : 'FrontPage',
-	mdroot : './',
-	mdtrash : './',
-	libpath : './libs',
-	mdctrl : function(){ 
-				var ctrl = require(this.libpath+'/libmd');
-				ctrl.init(this.mdroot,this.mdtrash);
-				return ctrl;
-			},
-	snctrl : function(){
-				var ctrl = require(this.libpath+'/libfbsession');
-				return ctrl;
-			},
-	render : function (res, ejs_name, data_obj){
-				this.mdctrl().init(this.mdroot,this.mdtrash);
-				var categories = this.mdctrl().find_dirs("/");
-				var default_obj = {'categories':categories};
-				for(i in data_obj){
-					default_obj[i] = data_obj[i];
-				}
-				res.render(ejs_name,default_obj);
-			}
+    front_title : '',
+    add_title : '글작성하기',
+    edit_title : '글수정하기',
+    front_page : 'FrontPage',
+    mdroot : './',
+    mdtrash : './',
+    libpath : './libs',
+    mdctrl : function(){ 
+                var ctrl = require(this.libpath+'/libmd');
+                ctrl.init(this.mdroot,this.mdtrash);
+                return ctrl;
+            },
+    snctrl : function(){
+                var ctrl = require(this.libpath+'/libfbsession');
+                return ctrl;
+            },
+    render : function (res, ejs_name, data_obj){
+                this.mdctrl().init(this.mdroot,this.mdtrash);
+                var categories = this.mdctrl().find_dirs("/");
+                var default_obj = {'categories':categories};
+                for(i in data_obj){
+                    default_obj[i] = data_obj[i];
+                }
+                res.render(ejs_name,default_obj);
+            }
 }
 
 var conf = require(pre_obj.libpath+'/libconf');
