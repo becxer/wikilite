96cb590860f7e70f316068d2d6d55e37ed8f528b
becxer <becxer87@gmail.com>
2017-12-11 00:36:17 +0900

    add posts

--- wikilite-server.py
+++ wikilite-server.py
@@ -13,22 +13,25 @@ config = load_json_from_file("config.json")
 userinfo = load_json_from_file("userinfo.json")
 print("config : ", config)
 
-source_root = config["source-root"]
-if not os.path.exists(source_root):
-    os.makedirs(source_root)
+library_root = config["library-root"]
+if not os.path.exists(library_root):
+    os.makedirs(library_root)
 
 comment_root = config["comment-root"]
 if not os.path.exists(comment_root):
     os.makedirs(comment_root)
-    
-def update_source():
-    source_giturls = config["source-giturls"]
+
+library_dir = {library_root : {}}
+def update_library(root):
+    global library_root
+    source_giturls = config["library-giturls"]
     print("source updating...")
     for giturl in tqdm(source_giturls):
         title = giturl.split("/")[-1]
         if ".git" in title:
             title = title[:-4]
-        tab_path = os.path.join(source_root, title)
+        root[title] = {}
+        tab_path = os.path.join(library_root, title)
         if not os.path.exists(tab_path):
             process = subprocess.Popen(["git", "clone", giturl, tab_path])
             output = process.communicate()[0]
@@ -38,19 +41,39 @@ def update_source():
             process = subprocess.Popen(["git", "-C", tab_path, "reset", "--hard", "origin/master"])
             output = process.communicate()[0]
 
-update_source()
+update_library(library_dir[library_root])
+print(library_dir)
+
+def parse_all_mds(root):
+    global library_root
+    for book_name in root.keys():
+        book_root = os.path.join(library_root, book_name)
+        book_file_list = os.listdir(book_root)
+        for fname in book_file_list:
+            fpath = os.path.join(book_root, fname)
+            if fname.lower() == "readme.md" and os.path.isfile(fpath):
+                readme_path = fpath
+                print(readme_path)
+            elif fname == "_posts" and os.path.isdir(fpath):
+                book_page_list = os.listdir(fpath)
+                for page_name in book_page_list:
+                    page_path = os.path.join(fpath, page_name)
+                    print(page_path)
+            
+        
+parse_all_mds(library_dir[library_root])
 sys.exit()
 
 @app.route("/")
 def index():
     return "Index!"
 
-@app.route("/tab/<string:tab_name>/")
-def tab(tab_name):
+@app.route("/book/<string:book_name>/")
+def book(book_name):
     return
 
-@app.route("doc/<string:tab_name>/<string:doc_name>/")
-def doc(tab_name, doc_name):
+@app.route("/page/<string:book_name>/<string:page_name>/")
+def page(book_name, page_name):
     return
 
 if __name__ == "__main__":
